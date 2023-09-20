f68dc07b217ecb04c4f38d879634ee3854cc708c
larisabolanos <laribs14@gmail.com>
2023-09-19 21:07:22 -0600

    Agregar archivos de proyecto.

---
+++ TiposDatos1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDatos1
+{
+    internal class Program
+    {
+        struct Auto
+        {
+            public string marca;
+            public int modelo;
+            public string tipo;
+        }
+        static void Main(string[] args)
+        {
+            //tipos de datos
+            int num1, num2;
+            long larga = 588;
+            byte b = 125;
+            short s = 255;
+            float flotante = 3.14f;
+            double doble = 25.568899d;
+            decimal deci = 255.36m;
+            Boolean boleano = false;
+            char caracter = 'a';
+            string cadena = "Hoy es lunes";
+
+            //var dynamic 
+
+            var variable = 20;
+            dynamic dinamico = "esto es C#";
+            dinamico = true;
+            dinamico='b';
+            dinamico = 2.14f;
+
+            Auto carro;
+            carro.marca = "BMW";
+            carro.tipo = "Sedan";
+            carro.modelo = 2024;
+
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("Marca:" + "Tipo:" + "Modelo: ");
+            Console.WriteLine("-------------------------");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(carro.marca + "  "+ carro.tipo + "   " + carro.modelo);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Marca:{0} Tipo: {1} Modelo: { 2}", carro.marca, carro.tipo, carro.modelo);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"Carro:{carro.marca}\nTipo:{carro.tipo}\nModelo{carro.modelo}");
+
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("--------------");
+            
+            num1 = 20; num2 = 5;
+
+            Console.WriteLine($"El rsultado de la suma es:{num1 + num2}");
+            Console.WriteLine("La multiplicacion es: {0}", num1 * num2);
+            Console.WriteLine("Digite un numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite un numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int resultado = num1 - num2;
+            int resultado2 = num1 / num2;
+
+            Console.WriteLine(flotante+doble);
+            float resultado3 = flotante + float.Parse(doble.ToString());
+
+            Console.WriteLine($"La resta es de {resultado}");
+            Console.WriteLine($"La division es de {resultado2}");
+
+            int[] notas = { 100, 80, 90, 78 };
+            string[] estudiantes = new string[4];
+            estudiantes[0] = "Luis";
+            estudiantes[1] = "Maria";
+            estudiantes[2] = "Karla";
+            estudiantes[3] = "Carlos";
+
+            for (int i = 0; i <=3; i++)
+            {
+                Console.WriteLine(notas[i]);
+
+            }
+
+
+
+             Console.ReadLine();
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ TiposDatos1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("TiposDatos1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TiposDatos1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("feac9ea9-c13a-44c4-9886-6a085405133f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
